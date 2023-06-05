c022bcdd854edb4289ad4d3aab0d53ad58643073
Vitaly Vetrof <vetrof@gmail.com>
2023-06-05 14:33:08 +0600

    init

--- hello/hello_project/settings.py
+++ hello/hello_project/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # usr app
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
