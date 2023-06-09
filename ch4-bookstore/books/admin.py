c76ecec2349f6be6fee69edb92d80f3756871c47
Vitaly Vetrof <vetrof@gmail.com>
2023-06-09 15:20:57 +0600

    review

--- ch4-bookstore/books/admin.py
+++ ch4-bookstore/books/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from books.models import Book
+from books.models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = (ReviewInline,)
     list_display = ('title', 'author', 'price')
 
 
 admin.site.register(Book, BookAdmin)
+
+admin.site.register(Review)
