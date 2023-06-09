d3cee5d1d4095153b75e68cdf3e389f7eca2d83a
Vitaly Vetrof <vetrof@gmail.com>
2023-06-09 20:17:57 +0600

    login required

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from books.models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'book_list'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     template_name = 'book/book_detail.html'
     context_object_name = 'book'
+    login_url = 'account_login'
+    permission_required = 'books.special_status'
 
 
