d0d817a6f0347c79abc868666203144e44e3eb50
Vitaly Vetrof <vetrof@gmail.com>
2023-06-09 17:08:04 +0600

    media

--- ch4-bookstore/django_project/urls.py
+++ ch4-bookstore/django_project/urls.py
@@ -1,9 +1,9 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
     # Django admin
     path("admin/", admin.site.urls),
 
@@ -13,5 +13,4 @@ urlpatterns = [
     # Local apps
     path("", include("pages.urls")),
     path('books/', include('books.urls'))
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
